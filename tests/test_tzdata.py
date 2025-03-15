ff250dc2225bbd1e7a22463fc93e933339d21976
w3stling <peter.westling@gmail.com>
2025-03-15 07:45:26 +0000

    Fix

--- tests/test_tzdata.py
+++ tests/test_tzdata.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 
+
+@pytest.fixture
 def test_tzdata_path():
     assert 'PYTHONTZPATH' in os.environ, "PYTHONTZPATH is not set"
     print(f"PYTHONTZPATH: {os.environ['PYTHONTZPATH']}")
