db3463b6a8f86dac1ecb31ed1919b3824f015572
Marcus Read <marcusaread@gmail.com>
2025-09-26 17:05:48 +0100

    Fix test class TestXLITExchangeCalendar

--- tests/test_xlit_calendar.py
+++ tests/test_xlit_calendar.py
@@ -1,20 +1,28 @@
 import pytest
+
 from exchange_calendars.exchange_calendar_xlit import XLITExchangeCalendar
 
+from .test_exchange_calendar import ExchangeCalendarTestBase
 
-class TestXLITExchangeCalendar:
+
+
+
+
+
+
+class TestXLITExchangeCalendar(ExchangeCalendarTestBase):
     @pytest.fixture(scope="class")
-    def calendar(self):
-        return XLITExchangeCalendar()
+    def calendar_cls(self):
+        return XLITExchangeCalendar
 
     @pytest.fixture
     def max_session_hours(self):
         # The XLIT is open from 10:00 AM to 4:00 PM on its longest trading day.
         yield 6
 
-    @pytest.mark.parametrize(
-        "date",
-        [
+    @pytest.fixture
+    def regular_holidays_sample(self):
+        yield [
             "2024-01-01",  # New Year's Day
             "2024-02-16",  # Restoration of the State
             "2024-03-11",  # Restoration of Independence
@@ -29,22 +37,14 @@ class TestXLITExchangeCalendar:
             "2024-12-24",  # Christmas Eve
             "2024-12-26",  # Boxing Day
             "2024-12-31",  # New Year's Eve
-        ],
-    )
-    def test_regular_holidays(self, calendar, date):
-        cal = calendar.regular_holidays.holidays(start=date, end=date)
-        assert date in cal.strftime("%Y-%m-%d").tolist()
+        ]
 
-    @pytest.mark.parametrize(
-        "date",
-        [
+    @pytest.fixture
+    def non_holidays_sample(self):
+        yield [
             "2024-01-02",  # Not a holiday
             "2024-03-12",  # Not a holiday
             "2024-04-02",  # Not a holiday
             "2024-05-02",  # Not a holiday
             "2024-06-25",  # Not a holiday
-        ],
-    )
-    def test_non_holidays(self, calendar, date):
-        cal = calendar.regular_holidays.holidays(start=date, end=date)
-        assert date not in cal.strftime("%Y-%m-%d").tolist()
+        ]
