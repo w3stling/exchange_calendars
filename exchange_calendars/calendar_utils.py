15611cd8e212f473aa148d0ccb7e4c40a0845191
w3stling <peter.westling@gmail.com>
2026-01-17 10:36:47 +0000

    Add Eurex stock exchange

--- exchange_calendars/calendar_utils.py
+++ exchange_calendars/calendar_utils.py
@@ -28,6 +28,8 @@ from .exchange_calendar_xdub import XDUBExchangeCalendar
 from .exchange_calendar_xdus import XDUSExchangeCalendar
 from .exchange_calendar_xeee import XEEEExchangeCalendar
 from .exchange_calendar_xetr import XETRExchangeCalendar
+from .exchange_calendar_xeur import XEURExchangeCalendar
+
 from .exchange_calendar_xfra import XFRAExchangeCalendar
 from .exchange_calendar_xham import XHAMExchangeCalendar
 from .exchange_calendar_xhel import XHELExchangeCalendar
@@ -100,6 +102,7 @@ _default_calendar_factories = {
     "XEEE": XEEEExchangeCalendar,
     "XFRA": XFRAExchangeCalendar,
     "XETR": XETRExchangeCalendar,
+    "XEUR": XEURExchangeCalendar,
     "XHAM": XHAMExchangeCalendar,
     "XHEL": XHELExchangeCalendar,
     "XHKG": XHKGExchangeCalendar,
